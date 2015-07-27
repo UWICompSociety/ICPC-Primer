e86c267ff7af60947baec11ac9d9f9c0df3998dd
Travis Smith <travissmith94@hotmail.com>
2015-07-26 19:37:31 -0500

    Adding problem

---
+++ Introduction/AddingProblem/AddingProblem.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class AddingProblem{
+	static Scanner scan = new Scanner(System.in);
+	public static void main(String[] args){
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		System.out.print(a + b);
+	}
+		
+}
